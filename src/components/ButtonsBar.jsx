69b7e6d5bbd659805886bec3fe9b0b412a973976
chocoisntlate <130369559+chocoisntlate@users.noreply.github.com>
2025-08-24 22:00:09 +0700

    cleaner

--- src/components/ButtonsBar.jsx
+++ src/components/ButtonsBar.jsx
@@ -2,7 +2,7 @@ import { PDFDownloadLink } from "@react-pdf/renderer"
 
 export default function ButtonsBar({setDisplayStatus, children}) {
     return (
-        <div>
+        <div className="buttons-bar">
             <button onClick={()=> {setDisplayStatus(false)}}>Edit</button>
             <PDFDownloadLink document={children} fileName="resume.pdf">
                 {({ loading }) =>
